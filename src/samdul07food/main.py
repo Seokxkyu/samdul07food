ed9f3ec3204091d86a1fd13af21e5176f5a8d048
Seokxkyu <kyuseok000@gmail.com>
2024-09-13 10:32:45 +0900

    main

--- src/samdul07food/main.py
+++ src/samdul07food/main.py
@@ -1,8 +1,8 @@
 from typing import Union
 from fastapi import FastAPI
-import pickle
 import pandas as pd
 from datetime import datetime
+import os
 
 app = FastAPI()
 
@@ -10,6 +10,7 @@ app = FastAPI()
 def get_path():
     file_path = __file__
     dirpath = os.path.dirname(file_path)
+    print(dirpath)
     return dirpath
 
 @app.get("/")
@@ -23,7 +24,7 @@ def food(name: str):
     time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     
     df = pd.DataFrame([[name, time]], columns=['food', 'time'])
-    
+    '''
     dir_path = get_path()    
     file_path = os.path.join(dir_path, 'food07.csv')
     
@@ -31,6 +32,13 @@ def food(name: str):
     
     # 음식 이름과 시간을 csv로 저장 -> /code/data/food.csv
     df.to_csv(file_path, mode='a', header=False, index=False)
-
+    '''
+    home_path = os.expand.user('~')
+    file_path = 'code/data/food7.csv'
+    csv_path = os.path.join(home_path, f'{file_path}')
+    
+    os.makedirs(csv_path, exist_ok=True)
+    
+    df.to_csv(csv_path, mode='a', header=False, index=False)
+    
     return {"food": name, "time": time}
-
