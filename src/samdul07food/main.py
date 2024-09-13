9615b40530b2f94fe3576723aa4d1ecfb75b2163
Seokxkyu <kyuseok000@gmail.com>
2024-09-13 14:15:15 +0900

    origin

--- src/samdul07food/main.py
+++ src/samdul07food/main.py
@@ -8,7 +8,18 @@ import pandas as pd
 app = FastAPI()
 
 origins = [
-    "http://127.0.0.1:8899"
+    "http://127.0.0.1:8899",
+    "http://localhost",
+    "http://localhost:8899",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 ]
 
 app.add_middleware(
